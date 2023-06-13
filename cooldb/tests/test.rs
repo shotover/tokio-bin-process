f4d0d92210a85ba43654b2811a619302086673d3
Lucas Kent <rubickent@gmail.com>
2023-06-13 13:07:32 +1000

    Heavily refactor API to support non-workspace crates and arbitrary binaries (#7)

--- cooldb/tests/test.rs
+++ cooldb/tests/test.rs
@@ -1,13 +1,14 @@
 use std::time::Duration;
 use tokio::time::timeout;
+use tokio_bin_process::bin_path;
 use tokio_bin_process::event::Level;
 use tokio_bin_process::event_matcher::EventMatcher;
 use tokio_bin_process::BinProcess;
 
 #[tokio::test(flavor = "multi_thread")]
-async fn test_cooldb_by_crate_name() {
+async fn test_cooldb() {
     // Setup cooldb
-    let mut cooldb = cooldb(None).await;
+    let mut cooldb = cooldb().await;
 
     // Assert that some functionality occured.
     // Use a timeout to prevent the test hanging if no events occur.
@@ -20,33 +21,12 @@ async fn test_cooldb_by_crate_name() {
                 .with_message("some functionality occurs"),
         );
 
-    // Shutdown cooldb asserting that it encountered no errors
     cooldb.shutdown_and_then_consume_events(&[]).await;
 }
 
-#[tokio::test(flavor = "multi_thread")]
-async fn test_cooldb_by_crate_name_bench_profile() {
-    // Setup cooldb with custom profile
-    let mut cooldb = cooldb(Some("bench")).await;
-
-    // Assert that some functionality occured.
-    // Use a timeout to prevent the test hanging if no events occur.
-    timeout(Duration::from_secs(5), cooldb.consume_events(1, &[]))
-        .await
-        .unwrap()
-        .assert_contains(
-            &EventMatcher::new()
-                .with_level(Level::Info)
-                .with_message("some functionality occurs"),
-        );
-
-    // Shutdown cooldb asserting that it encountered no errors
-    cooldb.shutdown_and_then_consume_events(&[]).await;
-}
-
-async fn cooldb(profile: Option<&'static str>) -> BinProcess {
+async fn cooldb() -> BinProcess {
     let mut cooldb =
-        BinProcess::start_crate_name("cooldb", "cooldb", &["--log-format", "json"], profile).await;
+        BinProcess::start_binary(bin_path!("cooldb"), "cooldb", &["--log-format", "json"]).await;
 
     timeout(
         Duration::from_secs(30),
--- tokio-bin-process/single-crate-test/tests/test.rs
+++ tokio-bin-process/single-crate-test/tests/test.rs
@@ -1,13 +1,29 @@
 use std::time::Duration;
+use tokio_bin_process::bin_path;
 use tokio_bin_process::BinProcess;
 
 #[tokio::test(flavor = "multi_thread")]
-async fn test_cooldb() {
-    let process = BinProcess::start_with_args(
+async fn test_cooldb_crate_name() {
+    let process = BinProcess::start_crate_name(
         "single-crate-test",
         "cooldb",
         &["--log-format", "json"],
         None,
+    )
+    .await;
+
+    // give the process time to setup its sigint/sigterm handler
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    process.shutdown_and_then_consume_events(&[]).await;
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_cooldb_binary() {
+    let process = BinProcess::start_binary(
+        bin_path!("single-crate-test"),
+        "cooldb",
+        &["--log-format", "json"],
     )
     .await;
 
